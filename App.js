ab06e67bcaa35d389c9b13c071e55ecf69c2ebd2
lee-tricia <80332823+lee-tricia@users.noreply.github.com>
2021-06-16 00:36:16 +0800

    Update App to start at login page

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import loginStack from "./loginStack";
+import signupPatient from "./signupPatient";
+import patientApp from "./PatientScreens/patientApp"
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator headerMode="none" mode="modal">
+        <Stack.Screen name="SignOut" component={loginStack} />
+        <Stack.Screen name="signupPatient" component={signupPatient} />
+        <Stack.Screen name="patientApp" component={patientApp} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
